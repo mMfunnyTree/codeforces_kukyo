cd07db44196af9e1816002d5ada97e2fd73daf0c
qujing.kukyo <qujing.oykuk@bytedance.com>
2020-07-11 19:53:26 +0800

    readme

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  codeforces_kukyo
+//
+//  Created by qujing.kukyo on 7/11/20.
+//  Copyright © 2020 qujing.kukyo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
